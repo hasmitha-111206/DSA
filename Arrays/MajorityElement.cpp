cafc9d75b57570b715a961ba1dc051d3f9241482
Hasmitha <hasmithakummaragunta@gmail.com>
2026-06-30 15:20:49 +0530

    Solved Majority Element

---
+++ Arrays/MajorityElement.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+class solution
+{
+public:
+    int majorityelement(vector<int> &nums)
+    {
+        int count = 0;
+        int candidate = 0;
+        for (int num : nums)
+        {
+            if (count == 0)
+            {
+                candidate = num;
+            }
+            if (num == candidate)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
