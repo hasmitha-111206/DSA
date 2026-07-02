b839bd55d6151a33fb857f2eb6d0a428033e5537
Hasmitha <hasmithakummaragunta@gmail.com>
2026-07-02 11:58:16 +0530

    Move Zeroes

---
+++ Arrays/MoveZeroes.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+class solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++)
+        {
+            if (nums[j] != 0)
+            {
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        }
+    }
+};
